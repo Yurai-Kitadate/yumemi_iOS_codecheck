6c84a2113d38d6f62e05a380fe341154ec1e9986
Yurai-Kitadate <yuurai18@gmail.com>
2023-03-30 10:00:50 +0900

    GitHubAPIClientのrepoをオプショナルに、UITestとUnitTestを追加

--- iOSEngineerCodeCheckUITests/iOSEngineerCodeCheckUITests.swift
+++ iOSEngineerCodeCheckUITests/iOSEngineerCodeCheckUITests.swift
@@ -8,36 +8,36 @@
 
 import XCTest
 
-class iOSEngineerCodeCheckUITests: XCTestCase {
-
+class iOSEngineerCodeCheckUITestsclass: XCTestCase {
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        XCUIApplication().launch()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    
+    func testSearchBar() throws {
         let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+        let searchBar = app.searchFields["GitHubのリポジトリを検索できるよー"]
+        searchBar.tap()
+        searchBar.typeText("swift")
+        app.keyboards.buttons["Search"].tap()
+        sleep(3)
+        
+        let tablesQuery1 = app.tables
+        XCTAssertTrue(tablesQuery1.count > 0)
+        
+        let cellIndexPath = IndexPath(row: 0, section: 0)
+        let cell = app.tables.cells.element(boundBy: cellIndexPath.row)
+        
+        sleep(3)
+        
+        cell.tap()
+        
+        app.navigationBars.buttons.element(boundBy: 0).tap()
+        
+        let tablesQuery2 = app.tables
+        XCTAssertTrue(tablesQuery2.count > 0)
+        
     }
 }
+
